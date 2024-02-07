70f07ba7ad43e545e08931fa0c4f091a79722ad8
ghosty47 <ghostcybertron@gmail.com>
2024-02-07 11:43:55 +0100

    initialized project

---
+++ lib/homepage.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:my_food_app/onboarding.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Future.delayed(
+      const Duration(seconds: 10),
+      () => Navigator.push(context, MaterialPageRoute(builder: (context) => const Onboarding())),
+    );
+    return const Scaffold(
+      body: Center(
+        child: Image(
+          image: AssetImage('assets/images/logo.png'),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/onboarding.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class Onboarding extends StatefulWidget {
+  const Onboarding({super.key});
+
+  @override
+  State<Onboarding> createState() => _OnboardingState();
+}
+
+class _OnboardingState extends State<Onboarding> {
+  final amina = PageController();
+  bool any = false;
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        controller: amina,
+        onPageChanged: (index) {
+          setState(() => );
+        },
+      ),
+    );
+  }
+}
